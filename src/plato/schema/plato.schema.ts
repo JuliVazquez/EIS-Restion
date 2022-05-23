b5bbd0c094df7171e87a5293aaa2e2def6e97719
Julian Vazquez <73852125+JuliVazquez@users.noreply.github.com>
2022-05-22 21:09:37 -0300

    agregado imagen de  plato

--- src/plato/schema/plato.schema.ts
+++ src/plato/schema/plato.schema.ts
@@ -3,5 +3,6 @@ import { Schema } from "mongoose";
 
 export const PlatoSchema = new Schema({
     nombre : {type: String, required: true},
-    precio : {type: Number, required: true}
+    precio : {type: Number, required: true},
+    imagen : {type: String, required: true}
 });
